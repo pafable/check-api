bb4b11a1a78e775e781c16d141d90d9eb45c13c3
pafable <39135798+pafable@users.noreply.github.com>
2022-10-01 23:54:21 -0400

    Dev/dev 10 (#13)
    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='check-api',
+    version='0.0.1',
+    description="phil's test project",
+    url='https://github.com/pafable/check-api',
+    author='pafable',
+    packages=setuptools.find_packages()
+)
